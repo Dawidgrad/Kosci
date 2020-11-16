1c47e3fda5b469a7ec993c610191a3fbea364293
Dawid Grad <dawidgrad.work@gmail.com>
2020-11-16 23:32:45 +0000

    Retrieve a user record / Prettier changes

---
+++ resources/js/home.js
@@ -0,0 +1,23 @@
+import {http} from http;
+const mongoose = require('mongoose');
+
+const url = 'mongodb+srv://dawidgrad:<password>@cluster0.vbira.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// Connect to the Mongo database
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// Define schema
+const User = mongoose.model('user', {
+	name: String,
+	wins: Number,
+	losses: Number,
+});
+
+const server = http.createServer((request, response) => {
+	User.find({}, (err, docs) => {
+		response.setHeader('content-type', 'text/json');
+		const result = JSON.stringify({ user: docs });
+		$('#queryResult').html(result);
+		response.end(result);
+	});
+});

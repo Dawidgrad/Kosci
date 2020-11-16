642a97a657d5ae3b77090e1b85c965cfae30614e
Dawid Grad <dawidgrad.work@gmail.com>
2020-11-16 19:43:37 +0000

    Added server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const port = 9000;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+	const urlParts = url.parse(req.url, true);
+
+	const permittedFiles = /(.html|.js)/gi;
+
+	if (urlParts.path.search(permittedFiles) !== -1) {
+		fs.readFile(__dirname + urlParts.path, (err, file) => {
+			if (urlParts.path.indexOf('.html')) {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/javascript' });
+			}
+
+			res.end(file);
+		});
+	} else {
+		res.writeHead(404);
+		res.end();
+	}
+});
+
+server.listen(port, () => {
+	console.log(`Server listening on ${port}`);
+});

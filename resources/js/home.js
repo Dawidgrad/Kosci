7d49430fd4a6ad6e1519b0ef9f59096a3e69799c
Dawid Grad <dawidgrad.work@gmail.com>
2020-11-24 23:13:21 +0000

    Added basic login and register page

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,23 +1,4 @@
-const http = require('http');
-const mongoose = require('mongoose');
-
-const url = 'mongodb+srv://dawidgrad:<password>@cluster0.vbira.mongodb.net/<dbname>?retryWrites=true&w=majority';
-
-// Connect to the Mongo database
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
-
-// Define schema
-const User = mongoose.model('user', {
-	name: String,
-	wins: Number,
-	losses: Number,
-});
-
-const server = http.createServer((request, response) => {
-	User.find({}, (err, docs) => {
-		response.setHeader('content-type', 'text/json');
-		const result = JSON.stringify({ user: docs });
-		$('#queryResult').html(result);
-		response.end(result);
-	});
+$(() => {
+	// Request the data
+	// Update the result paragraph
 });

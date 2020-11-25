ecc25b87f3a4915217db1672b4efeffb851dcdd1
Dawid Grad <dawidgrad.work@gmail.com>
2020-11-25 23:35:31 +0000

    Using ExpressJS

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({ nickname: String, password: String });
+// Schemas
+const userSchema = new mongoose.Schema({ name: String, wins: Number, losses: Number }, { collection: 'koscidb' });
+
+// Models
 const User = mongoose.model('user', userSchema);
 
+// Functions
 async function listAllUsers() {
 	const users = await User.find({});
 	return users;
 }
 
 module.exports.listAllUsers = listAllUsers;
+module.exports.User = User;
--- server.js
+++ server.js
@@ -1,32 +1,45 @@
 const express = require('express');
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const mongoose = require('mongoose');
+const path = require('path');
+const db = require('./db');
+const { User } = require('./db');
 
+// Specify the port
 const port = process.env.PORT || 9000;
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
-	const urlParts = url.parse(req.url, true);
+// Connect MongoDB
+const MONGODB_URI = 'mongodb+srv://dawidgrad:YubiYubi@university.vbira.mongodb.net/koscidb?retryWrites=true&w=majority';
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
-	const permittedFiles = /(.html|.js)/gi;
+// Add some data
+// const data = {
+// 	name: 'Dawid',
+// 	wins: 10,
+// 	losses: 1,
+// };
+// const newUser = new User(data);
+// newUser.save((error) => {
+// 	if (error) {
+// 		console.log('Could not add the user');
+// 	} else {
+// 		console.log('Successfully added the user!');
+// 	}
+// });
 
-	if (urlParts.path.search(permittedFiles) !== -1) {
-		fs.readFile(__dirname + urlParts.path, (err, file) => {
-			if (urlParts.path.indexOf('.html')) {
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-			} else {
-				res.writeHead(200, { 'Content-Type': 'text/javascript' });
-			}
+const app = express();
 
-			res.end(file);
-		});
-	} else {
-		res.writeHead(404);
-		res.end();
-	}
+// Set the view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/users', async (request, response) => {
+	const users = await db.listAllUsers();
+	const data = { retrievedUsers: users };
+	response.json(data);
+	// response.render('pagename', data) in case of a view - ejs file
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
 	console.log(`Server listening on ${port}`);
 });

04eac274d9172dce70ec4e255434390e11e6ffdc
Dawid Grad <dawidgrad.work@gmail.com>
2020-12-05 23:43:45 +0000

    Fixed a bug where listeners were being set up multiple times

--- routes/game.js
+++ routes/game.js
@@ -3,11 +3,20 @@ const roomController = require('../controllers/room_controller');
 
 const router = express.Router();
 
+let listenersSetUp = false;
+
 router.get('/', (req, res) => {
 	if (req.session.loggedIn) {
 		res.render('game');
 		const io = res.app.get('io');
+		setUpListeners(io);
+	} else {
+		res.redirect('/login');
+	}
+});
 
+function setUpListeners(io) {
+	if (listenersSetUp == false) {
 		io.on('connection', (socket) => {
 			socket.on('create room', async (roomSize) => {
 				const name = await roomController.createRoom(roomSize);
@@ -26,9 +35,7 @@ router.get('/', (req, res) => {
 				io.in(roomName).emit('game started');
 			});
 		});
-	} else {
-		res.redirect('/login');
 	}
-});
+}
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
+let listenersSetUp = false;
+
 router.get('/', (req, res) => {
 	if (req.session.loggedIn) {
 		res.render('home');
-		const io = res.app.get('io');
+		io = res.app.get('io');
+		setUpSocketListeners(io);
+	} else {
+		res.redirect('/login');
+	}
+});
 
+function setUpSocketListeners(io) {
+	if (listenersSetUp == false) {
 		io.on('connection', (socket) => {
 			socket.on('join server', (username) => {
 				console.log(`${username} joined the server`);
 			});
 		});
-	} else {
-		res.redirect('/login');
+		listenersSetUp = true;
 	}
-});
+}
 
 module.exports = router;
